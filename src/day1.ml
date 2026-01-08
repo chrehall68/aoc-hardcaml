d338ba16739a4fdc29169c178e3f2814b9532fc0
chrehall68 <chrehall68@gmail.com>
2026-01-08 10:45:49 -0800

    initialize repo; start day1

---
+++ src/day1.ml
@@ -0,0 +1,94 @@
+(* An example design that takes a series of input values and calculates the range between
+   the largest and smallest one. *)
+
+(* We generally open Core and Hardcaml in any source file in a hardware project. For
+   design source files specifically, we also open Signal. *)
+open! Core
+open! Hardcaml
+open! Signal
+
+let num_bits = 32
+
+(* Every hardcaml module should have an I and an O record, which define the module
+   interface. *)
+module I = struct
+  type 'a t =
+    { clock : 'a
+    ; clear : 'a
+    ; start : 'a
+    ; finish : 'a
+    ; dial_amt : 'a [@bits num_bits]
+    ; is_left : 'a
+    ; data_in_valid : 'a
+    }
+  [@@deriving hardcaml]
+end
+
+module O = struct
+  type 'a t =
+    { (* With_valid.t is an Interface type that contains a [valid] and a [value] field. *)
+      zero_count : 'a With_valid.t [@bits num_bits]
+    }
+  [@@deriving hardcaml]
+end
+
+module States = struct
+  type t =
+    | Idle
+    | Accepting_inputs
+    | Done
+  [@@deriving sexp_of, compare ~localize, enumerate]
+end
+
+let create scope ({ clock; clear; start; finish; dial_amt; is_left; data_in_valid } : _ I.t) : _ O.t
+  =
+  let spec = Reg_spec.create ~clock ~clear () in
+  let open Always in
+  let sm =
+    State_machine.create (module States) spec
+  in
+  let%hw_var cur_dial = Variable.reg spec ~width:num_bits in
+  let%hw_var zero_counter = Variable.reg spec ~width:num_bits in
+  (* We don't need to name the range here since it's immediately used in the module
+     output, which is automatically named when instantiating with [hierarchical] *)
+  let zero_count = Variable.wire ~default:(zero num_bits) () in
+  let zero_count_valid = Variable.wire ~default:gnd () in
+  compile
+    [ sm.switch
+        [ ( Idle
+          , [ when_
+                start
+                [ 
+                  cur_dial <-- of_unsigned_int 50
+                ; zero_counter <-- of_unsigned_int 0
+                ; sm.set_next Accepting_inputs
+                ]
+            ] )
+        ; ( Accepting_inputs
+          , [ when_
+                data_in_valid
+                [ 
+                  if_ is_left [ cur_dial <-- (cur_dial - dial_amt) % (of_unsigned_int 100) ]
+                    [cur_dial <-- (cur_dial + dial_amt) % (of_unsigned_int 100)]
+                  ;
+                  when_ cur_dial == of_unsigned_int 0 [ zero_counter <-- zero_counter + 1 ]
+                ]
+            ; when_ finish [ sm.set_next Done ]
+            ] )
+        ; ( Done
+          , [ zero_count <-- zero_counter 
+            ; zero_count_valid <-- vdd
+            ; when_ finish [ sm.set_next Accepting_inputs ]
+            ] )
+        ]
+    ];
+  (* [.value] is used to get the underlying Signal.t from a Variable.t in the Always DSL. *)
+  { zero_count = { value = zero_count.value; valid = zero_count_valid.value } }
+;;
+
+(* The [hierarchical] wrapper is used to maintain module hierarchy in the generated
+   waveforms and (optionally) the generated RTL. *)
+let hierarchical scope =
+  let module Scoped = Hierarchy.In_scope (I) (O) in
+  Scoped.hierarchical ~scope ~name:"day1" create
+;;
---
+++ src/day1.mli
@@ -0,0 +1,32 @@
+open! Core
+open! Hardcaml
+
+val num_bits : int
+
+(* every clock cycle, we will take a new "line" of input
+where a "line" of input should be the direction (L/R, encoded as low or high)
+and an integer (although the AOC input is only up to 100, I allow up to 32 bits integers
+since my solution doesn't depend that much on the magnitude of the numbers)
+
+We'll also take start, finish, and clear.
+- start should make the state machine start accepting inputs
+- finish should make us output our results
+- clear should reset the state machine*)
+module I : sig
+  type 'a t =
+    { clock : 'a
+    ; clear : 'a
+    ; start : 'a
+    ; finish : 'a
+    ; dial_amt : 'a
+    ; is_left : 'a
+    ; data_in_valid: 'a
+    }
+  [@@deriving hardcaml]
+end
+
+module O : sig
+  type 'a t = { zero_count : 'a With_valid.t } [@@deriving hardcaml]
+end
+
+val hierarchical : Scope.t -> Signal.t I.t -> Signal.t O.t
